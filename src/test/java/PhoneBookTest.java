86525c67f5759681fbe44cf2d48e424c2cd5a890
Fyodor Parfyonov <parfyonov.fyodor@gmail.com>
2023-08-20 17:04:52 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,24 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
-    
+    @Test
+    public void testAddNew() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Pavel", "00001");
+        int actual = phoneBook.add("Sveta", "00002");
+        int expected = 2;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddSame() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Pavel", "00001");
+        phoneBook.add("Sveta", "00002");
+        int actual = phoneBook.add("Pavel", "00003");
+        int expected = 2;
+        Assertions.assertEquals(expected, actual);
+    }
 }
